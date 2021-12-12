29f76f9aec5f580f567fb0b8051ecd76ed39f862
Amit Oz <ozamit7@gmail.com>
2021-12-12 16:33:49 +0100

    before the lesson 12.12.2021

--- employeeApp/src/app/list_socialbuttons.ts
+++ employeeApp/src/app/list_socialbuttons.ts
@@ -1,6 +1,8 @@
 import { socialbuttons } from '../app/socialbuttons';
 
 export const socialbtns: socialbuttons[] = [
-    { name: "facebook", icon: 'https://www.kindpng.com/picc/m/540-5402627_facebook-facebook-icon-white-background-hd-png-download.png', url: 'facebook.com'},
-    { name: "twitter", icon: 'https://i.pinimg.com/originals/b8/c8/46/b8c846a25866985a5719049b9f0c5890.png', url: 'twitter.com'}
+    { name: "facebook", icon: 'http://kzhandels.no/wp-content/uploads/2019/07/facebook-3-logo-png-transparent.png', url: 'facebook.com'},
+    { name: "twitter", icon: 'https://toppng.com/uploads/preview/format-twitter-logo-transparent-11549680770lolovrdq8m.png', url: 'twitter.com'},
+    { name: "instagram", icon: 'https://www.pinclipart.com/picdir/big/1-13590_instagram-logo-insta-logo-png-transparent-background-clipart.png', url: 'instagram.com'},
+    { name: "tiktok", icon: 'https://logosarchive.com/wp-content/uploads/2021/07/Tiktok-logo-icon-transparent.png', url: 'tiktok.com'}
 ]
